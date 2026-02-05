0bd1795fc988906ea7093bc49e8a8ba0d45411f7
Marcelo Bravo <mabc@live.cl>
2026-02-05 16:31:26 -0300

    Feat: Se implementó el componente footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Navbar } from './components/navbar/navbar';
-
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Header],
+  imports: [RouterOutlet, Navbar, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
